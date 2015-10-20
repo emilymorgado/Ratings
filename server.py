9190ccb45e832b63020da29419e37f555ba037c0
Hackbright Student <info@hackbrightacademy.com>
2015-10-20 12:35:54 -0700

    Created sign up page

--- server.py
+++ server.py
@@ -2,10 +2,10 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask
+from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import connect_to_db, db, User, Rating, Movie
 
 
 app = Flask(__name__)
@@ -22,7 +22,46 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     """Homepage."""
 
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template("homepage.html")
+
+
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+
+@app.route("/sign-up-form")
+def show_signup_form():
+
+    return render_template("create_account.html")
+
+
+@app.route("/new_account", methods=["POST"])
+def check_new_account():
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    existing_account = User.query.filter(User.email == email).first()
+
+    if (not existing_account) and (len(password) > 0):
+        flash("Your account has been created!")
+        user = User(email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/')
+    elif existing_account:
+        flash("That account already exists")
+        return redirect('/sign-up-form')
+    else:
+        flash("You need a password.")
+        return redirect('/sign-up-form')
+
+
+
 
 
 if __name__ == "__main__":
